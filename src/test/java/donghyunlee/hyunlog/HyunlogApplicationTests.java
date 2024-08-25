eded3dd1a71ecc8b2ed6b6587698242703ba6e1e
donghyunlee <suudh99@naver.com>
2024-08-25 18:26:39 +0900

    feat: init project (#HLG-8)

---
+++ src/test/java/donghyunlee/hyunlog/HyunlogApplicationTests.java
@@ -0,0 +1,13 @@
+package donghyunlee.hyunlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyunlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
