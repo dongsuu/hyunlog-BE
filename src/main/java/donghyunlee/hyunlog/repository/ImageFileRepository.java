815312b5e917b0efe0e66aac8fb9a69780b06fdf
donghyunlee <suudh99@naver.com>
2024-08-26 23:07:56 +0900

    feat: imageFileRepository 추가 (#HLG-12)

---
+++ src/main/java/donghyunlee/hyunlog/repository/ImageFileRepository.java
@@ -0,0 +1,9 @@
+package donghyunlee.hyunlog.repository;
+
+import donghyunlee.hyunlog.domain.ImageFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageFileRepository extends JpaRepository<ImageFile, Long> {
+}
