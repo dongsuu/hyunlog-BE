e91bb10bf78dd9843ca4d7b0fa29982d0a0203f7
donghyunlee <suudh99@naver.com>
2024-09-09 00:31:31 +0900

    feat: JpaAuditing annotation 추가 (#HLG-13)

--- src/main/java/donghyunlee/hyunlog/HyunlogApplication.java
+++ src/main/java/donghyunlee/hyunlog/HyunlogApplication.java
@@ -2,8 +2,10 @@ package donghyunlee.hyunlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HyunlogApplication {
 
 	public static void main(String[] args) {
